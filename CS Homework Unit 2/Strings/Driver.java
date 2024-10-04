5797714f2a33a11320e7bebcf1ff9d6c0cc4d57f
Nicholas Nesbitt <102713977+NichoJ23@users.noreply.github.com>
2024-10-04 13:29:59 -0700

    Add files via upload

--- CS Homework Unit 2/Strings/Driver.java
+++ CS Homework Unit 2/Strings/Driver.java
@@ -15,9 +15,30 @@ public class Driver {
         String lastLetter = StringTools.lastLetter(lastLetterInput);
         System.out.println("The last letter in " + lastLetterInput + " is " + lastLetter);
         
+        System.out.println("**formatPhoneNumber()**\nEnter a word: ");
+        String phoneNumberInput = input.nextLine();
+        String phoneNumber = StringTools.formatPhoneNumber(phoneNumberInput);
+        System.out.println("The formatted phone number is " + phoneNumber);
+        
         System.out.println("\n**middleThree()**\nEnter a word: ");
         String middleThreeInput = input.nextLine();
         String middleThree = StringTools.middleThree(middleThreeInput);
         System.out.println("The middle three letters in " + middleThreeInput + " are " + middleThree);
+        
+        System.out.println("\n**swapLastTwo()**\nEnter a word: ");
+        String swapLastTwoInput = input.nextLine();
+        String lastTwo = StringTools.swapLastTwo(swapLastTwoInput);
+        System.out.println("The new word is " + lastTwo);
+        
+        System.out.println("\n**frontAgain()**\nEnter a word: ");
+        String frontAgainInput = input.nextLine();
+        System.out.println("Enter a number of characters: ");
+        int chars = input.nextInt();
+        boolean frontAgain = StringTools.frontAgain(frontAgainInput, chars);
+        if (frontAgain) {
+            System.out.println("The first " + chars + " characters are repeated at the end of the word");
+        } else {
+            System.out.println("The first " + chars + " characters are not repeated at the end of the word");
+        }
     }
 }
