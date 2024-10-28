82fae0d64071c17519127333844a5d51d9095863
Nicholas Nesbitt <102713977+NichoJ23@users.noreply.github.com>
2024-10-28 13:36:17 -0700

    Problem Set 4C

---
+++ CS Homework Unit 4/PS4C/Driver.java
@@ -0,0 +1,10 @@
+/**
+ * AP Compsci 2024-2025
+ * Nicholas Nesbitt
+ * 
+ */
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println(Review.totalSentiment("ScientologyReview1.txt"));
+    }
+}
